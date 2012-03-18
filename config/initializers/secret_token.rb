faf26e31aa184d35a780c3d5a9b4b02ace920b5c
Christopher Bradford <bradfordcp@gmail.com>
2012-03-18 01:57:39 -0700

    Default project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Killboard::Application.config.secret_token = '15e3707a99ad300ab8627a0f9b80326a785d3ede90139bf2d37f4cab17b047da1b1c0ad0c7f91775cce0c4ac0a7d84e7de10b54280c089f65ce9566d7d375ff0'
